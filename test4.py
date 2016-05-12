5a9e055f43a1cb213fc993338c856203de0bc6ca
joel3gi <joel3gi@gmail.com>
2016-05-11 23:01:26 -0400

    Commented out sections for demo.

---
+++ test4.py
@@ -0,0 +1,14 @@
+#from random import choice
+#def main():
+    # Put cursor on "choice" and hold ALT - ENTER to automatically get imports
+    #for i in range(5):
+    #    print(choice([1,2,3,4,5,9]))
+#    main()
+
+# 1) start typing main to get this line:
+#if __name__ == '__main__':
+
+# 2) start typing main again to auto create the main function hold ALT - ENTER
+# main()
+
+# 3) replace the pass, with a loop using choice and hold ALT - ENTER to import
